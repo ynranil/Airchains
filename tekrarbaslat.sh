433667214115915c0ea664926e8d431d9d50604f
ynranil <137059762+ynranil@users.noreply.github.com>
2024-06-14 10:50:11 +0300

    Create tekrarbaslat.sh

---
+++ tekrarbaslat.sh
@@ -0,0 +1,14 @@
+!/bin/bash
+
+# Betiğin 5 dakikada bir çalışmasını sağlayan döngü
+while true; do
+  echo "Komut çalıştırılıyor: $(date)"
+
+  # Komutu çalıştır ve 5 dakika sonra durdur
+  timeout 5m ./tracks start
+
+  echo "Komut durduruldu: $(date)"
+
+  # 5 dakika bekle ve döngüyü tekrar başlat
+  sleep 300
+done
