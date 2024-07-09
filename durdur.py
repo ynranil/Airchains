eb38fa77cd7c3b19e62aeb58a7ec520338139fbc
ynranil <137059762+ynranil@users.noreply.github.com>
2024-07-09 10:33:21 +0300

    Update durdur.py

--- durdur.py
+++ durdur.py
@@ -6,10 +6,10 @@ import os
 while True:
     try:
         # Komutu başlat
-        process = subprocess.Popen(['./tracks', 'start'])
+        process = subprocess.Popen(['./tracks', 'start'], stderr=subprocess.PIPE)
         print("Komut çalıştırıldı, 10 dakika bekleniyor...")
 
-        # 2 dakika bekle
+        # 10 dakika bekle
         time.sleep(600)
 
         # Ctrl + C sinyali gönder (SIGINT)
@@ -18,10 +18,11 @@ while True:
 
         # Process'in sonlanmasını bekle
         process.wait()
-        print("Komut durduruldu, döngüye devam ediliyor...")
+        print("Komut durduruldu, işlemler kontrol ediliyor...")
 
-        # Eğer rpc error hatası varsa işlemleri gerçekleştir
-        stderr_output = process.stderr.read() if process.stderr else b''
+        # Stderr çıktısını al
+        stderr_output = process.stderr.read()
+
         if b'rpc error' in stderr_output:
             print("rpc error hatası algılandı, işlemler gerçekleştiriliyor...")
             # systemctl stop tracksd
@@ -29,8 +30,8 @@ while True:
             # 3 kez ./tracks rollback
             for _ in range(3):
                 subprocess.run(['./tracks', 'rollback'], check=True)
-            # ./tracks start
-            subprocess.run(['./tracks', 'start'], check=True)
+            # Python3 betiği durdur.py çalıştır
+            subprocess.run(['python3', 'durdur.py'], check=True)
             print("İşlemler tamamlandı, döngüye devam ediliyor...")
 
     except Exception as e:
